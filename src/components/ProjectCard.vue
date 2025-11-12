86dfdc6376179c0f051a81cc283a6a41fc50dcc6
rydellaforteza18 <rydellaforteza18@gmail.com>
2025-11-12 13:58:57 +0800

    sample portfolio

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<template>
+		
+	<div class="card col-md-3">
+
+		<img :src="project.image" class="card-img-top card-img">
+		<div>
+
+			<h5 class="card-title">{{project.title}}</h5>
+			<p class="card-text">{{project.description}}</p>
+
+		</div>
+
+	</div>
+
+</template>
+
+<script setup>
+
+	import { defineProps } from 'vue';
+
+	defineProps({
+		project: Object
+	})
+		
+</script>
